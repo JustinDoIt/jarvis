087782f4d561947933ad6b896fa905e81953bfe3
JustinDoIt <853103282@qq.com>
2021-07-07 14:38:39 +0800

    add api

--- api/pubmed.py
+++ api/pubmed.py
@@ -1,8 +1,17 @@
+from datetime import date
 from numpy import iterable
 import pandas as pd
 import requests
 import xml.etree.ElementTree as ET
 import time
+
+# from requests.api import get
+# import sys
+# sys.path.append("../")
+# from ..common.constants import *
+# from biomedical_text_mining.common.constants import *
+# from ..common.constants import REVEIVED_DAY_XPATH
+
 
 ###################################################
 # pubmed_query.py                 PubMed API的使用 #
@@ -18,7 +27,7 @@ import time
 ###################################################
 
 __all__ = [
-    "query_pubmed",
+    "query",
     "fetch_articles",
 ]
 
@@ -83,7 +92,7 @@ def query(query, output="json"):
         return response.text  # type(response.text) 是个 str
 
 ###################################################
-# 
+# fetch article content using pmids
 ###################################################
 
 def _get_content(article, xpath, default=None, separator="\n"):
@@ -96,58 +105,34 @@ def _get_content(article, xpath, default=None, separator="\n"):
     else:
         return separator.join([sub.text for sub in info if sub.text is not None])
 
-def _fetch_article(batech_ids):
+def _fetch_article(batch_ids):
+    """retrieves the 
+
+    Args:
+        batch_ids (list of pmids): article IDs
+    """
+    # parameters = self.parameters.copy() # 深拷贝的好处？
     parameters = {
             "tool": "my_tool",
             "email": "m13051995656@163.com",
             "db": "pubmed",
             "retmode": "XML",
-            "retmax": 50000,
+            "retmax": 500,
     }
+    parameters["id"] = batch_ids
 
-
-def fetch_articles(article_ids, xml_saved=False, xml_path="../_data_/", csv_saved=True, csv_path="../data/"):
-    """fetch scientific paper basic information with pmids. basic information inclus:
-    1. Title
-    2. Abstract
-    3. 
-
-    Args:
-        article_ids (list): ['33483714', '33361824', ... , '33318653']
-        xml_saved (bool, optional): whether save raw xml file. Defaults to False.
-        xml_path (str, optional): [description]. Defaults to "../_data_/".
-        csv_saved (bool, optional): [description]. Defaults to True.
-        csv_path (str, optional): whether save parsed csv file. Defaults to "../data/".
-
-    Returns:
-        df
-
-    Examples:
-        >>> 
-
-    
-    """
-
-    
-    # parameters["id"] = article_ids
-    articles = list([_fetch_article(
-        batech_ids=batch) for batch in _batches(article_ids, 250)]
-        )
+    # response = requests.get(
+    #     url="", params=parameters, output="xml"
+    # )
 
     BASE_URL = "https://eutils.ncbi.nlm.nih.gov"
     FUNCTION_URL = "/entrez/eutils/efetch.fcgi"
-
-    # 根据 id得到
     response = requests.get(BASE_URL+FUNCTION_URL, params=parameters)
     response.raise_for_status() # Check for any errors
-#     with open("tmp.xml", 'w') as f:
-#         f.write(article_xml)
-#     print(article_xml)
-    
-    # xml解析, 得到root迭代器
+
+
     root = ET.fromstring(response.text)
 
-    
     # title
     # TITTLE_XPATH = './PubmedArticle/MedlineCitation/Article/ArticleTitle'
     TITTLE_XPATH = ".//ArticleTitle"
@@ -155,7 +140,7 @@ def fetch_articles(article_ids, xml_saved=False, xml_path="../_data_/", csv_save
     # ABSTRACT_XPATH = './PubmedArticle/MedlineCitation/Article/Abstract/AbstractText'
     ABSTRACT_XPATH = ".//AbstractText"
     # doi
-    DOI_XPATH = './PubmedArticle/MedlineCitation/Article/ELocationID'
+    # DOI_XPATH = './PubmedArticle/MedlineCitation/Article/ELocationID'
     DOI_XPATH = ".//Article/ELocationID"
     # pmid
     # PMID_XPATH = './PubmedArticle/MedlineCitation/'  # 不work
@@ -184,11 +169,11 @@ def fetch_articles(article_ids, xml_saved=False, xml_path="../_data_/", csv_save
     JOURNAL_XPATH = ".//Journal/Title"
     JOURNAL_ABBREVIATION_XPATH = './PubmedArticle/MedlineCitation/Article/Journal/ISOAbbreviation'
     JOURNAL_ABBREVIATION_XPATH = ".//Article/Journal/ISOAbbreviation"
-    
+
     # publication_type
     PUBLICATION_TYPE_XPATH = './PubmedArticle/MedlineCitation/Article/PublicationTypeList/PublicationType'
     PUBLICATION_TYPE_XPATH = ".//PublicationType"
-    
+
     # author
     LASTNAME_XPATH = './PubmedArticle/MedlineCitation/Article/AuthorList/Author/LastName'
     LASTNAME_XPATH = ".//AuthorList/Author/LastName"
@@ -198,11 +183,11 @@ def fetch_articles(article_ids, xml_saved=False, xml_path="../_data_/", csv_save
     FORENAME_XPATH = ".//AuthorList/Author/ForeName"
     # affiliation
     AFFILIATION_XPATH = './/Author/AffiliationInfo'
-    
+
     # country
     COUNTRY_XPATH = './PubmedArticle/MedlineCitation/MedlineJournalInfo/Country'
     COUNTRY_XPATH = ".//MedlineJournalInfo/Country"
-    
+
     df = pd.DataFrame(columns=["Tittle", "abstract", "publication_type",
                                "publication_year", "publication_month", "publication_day",
                                "accepted_year", "accepted_month", "accepted_day",
@@ -210,6 +195,7 @@ def fetch_articles(article_ids, xml_saved=False, xml_path="../_data_/", csv_save
                               "journal", "journal_abbreviation", "lastname", "forename", "initials",
                                "affiliation", "country", "doi", "pmid",
                               ])
+
     for index, article in enumerate(root.iter("PubmedArticle")):
         df.loc[index] = {
             "Tittle": _get_content(article=article, xpath=TITTLE_XPATH),
@@ -234,17 +220,58 @@ def fetch_articles(article_ids, xml_saved=False, xml_path="../_data_/", csv_save
             "doi": _get_content(article=article, xpath=DOI_XPATH),
             "pmid": _get_content(article=article, xpath=PMID_XPATH),
         }
+    return df
+
+
+def fetch_articles(article_ids, xml_saved=False, xml_path="../_data_/", csv_saved=False, csv_path="../data/"):
+    """fetch scientific paper basic information with pmids. basic information inclus:
+    1. Title
+    2. Abstract
+    3. 
+
+    Args:
+        article_ids (list): ['33483714', '33361824', ... , '33318653']
+        xml_saved (bool, optional): whether save raw xml file. Defaults to False.
+        xml_path (str, optional): [description]. Defaults to "../_data_/".
+        csv_saved (bool, optional): [description]. Defaults to True.
+        csv_path (str, optional): whether save parsed csv file. Defaults to "../data/".
+
+    Returns:
+        df
+
+    Examples:
+        >>> 
+
+    
+    """
+    articles = pd.DataFrame()
+    for batch in _batches(article_ids, 250):
+        articles = articles.append(_fetch_article(batch)) # 每次增加一部分article信息
+    
     if csv_saved:
         SAVE_PATH = "./" + time.strftime("papers_%Y_%m_%d_%H:%M:%S".format(time.localtime()))  + ".csv"
-        df.to_csv(SAVE_PATH, index=0)
-    return df
+        articles.to_csv(SAVE_PATH, index=0)
+    return articles
 
 ###################################################
 # Test
 ###################################################
 def test_01():
     query = '("Nature biotechnology"[Journal]) AND (("2020/01/01"[Date - Publication] : "2020/12/31"[Date - Publication]))'
-    id_list = query_pubmed(query)
+    id_list = query(query)
 
     df = fetch_articles(id_list[:5])
     print(df)
+
+def test_02():
+    query_language = '("Nucleic acids research"[Journal]) AND (("2021/07/02"[Date - Publication] : "2021/07/03"[Date - Publication]))'
+    id_lists = query(query_language) # 95
+    print("Successfully query...")
+    df = fetch_articles(id_lists)
+    print(df)
+    print("Successfully...")
+
+if __name__ == "__main__":
+    test_02()
+    # from ..common.constants import * 
+    # print(REVEIVED_DAY_XPATH)
